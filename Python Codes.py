4d861529fbee2e2716d365edeb90a082a4692e9d
Richard R <richardrencn@163.com>
2015-11-12 19:37:07 -0500

    Initial commit

---
+++ Python Codes.py
@@ -0,0 +1,10 @@
+from visual import *
+from visual.graph import*
+
+#Setting up the Scene
+scene.width=800
+scene.height=800
+scene.range=(16, 16, 16)
+scene.autoscale = 0
+scene.center=(0, 12, 0)
+scene.background=color.white
